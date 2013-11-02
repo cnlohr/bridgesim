44e23052fd68d07dfc5f0fe35fa9baaa3659028f
cnlohr <charles@cnlohr.com>
2013-11-02 15:22:56 -0400

    stuff

--- src/graphics/test.c
+++ src/graphics/test.c
@@ -2,45 +2,44 @@
 #include <GL/freeglut.h>
 
 
-void
-reshape(int w, int h)
+void reshape(int w, int h)
 {
-  /* Because Gil specified "screen coordinates" (presumably with an
-     upper-left origin), this short bit of code sets up the coordinate
-     system to correspond to actual window coodrinates.  This code
-     wouldn't be required if you chose a (more typical in 3D) abstract
-     coordinate system. */
-
-  glViewport(0, 0, w, h);       /* Establish viewing area to cover entire window. */
-  glMatrixMode(GL_PROJECTION);  /* Start modifying the projection matrix. */
-  glLoadIdentity();             /* Reset project matrix. */
-  glOrtho(0, w, 0, h, -1, 1);   /* Map abstract coords directly to window coords. */
-  glScalef(1, -1, 1);           /* Invert Y axis so increasing Y goes down. */
-  glTranslatef(0, -h, 0);       /* Shift origin up to upper-left corner. */
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, w, 0, h, -1, 1);
+	glScalef(1, -1, 1);
+	glTranslatef(0, -h, 0);
 }
 
-void
-display(void)
+void display(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor( .2, .2, .2, 1 );
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glRasterPos2f(10, 100);
+	glColor3f( 1,0,1 );
+	glutBitmapString( GLUT_BITMAP_9_BY_15, "(15,51)" );
+
+/*
   glBegin(GL_TRIANGLES);
-    glColor3f(0.0, 0.0, 1.0);  /* blue */
+    glColor3f(0.0, 0.0, 1.0);
     glVertex2i(0, 0);
-    glColor3f(0.0, 1.0, 0.0);  /* green */
+    glColor3f(0.0, 1.0, 0.0);
     glVertex2i(200, 200);
-    glColor3f(1.0, 0.0, 0.0);  /* red */
+    glColor3f(1.0, 0.0, 0.0);
     glVertex2i(20, 200);
   glEnd();
-  glFlush();  /* Single buffered, so needs a flush. */
+*/
+	glFlush();
 }
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-  glutInit(&argc, argv);
-  glutCreateWindow("single triangle");
-  glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-  glutMainLoop();
-  return 0;             /* ANSI C requires main to return int. */
+	glutInit( &argc, argv );
+	glutCreateWindow( "Test" );
+	glutDisplayFunc( display );
+	glutReshapeFunc( reshape );
+	glutMainLoop();
+	return 0;
 }

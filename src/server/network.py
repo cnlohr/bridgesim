2120e2970c2ec6e4a6d9c4d28517a849e52e3318
thegeekguy <mark25@umbc.edu>
2013-11-02 23:42:35 -0400

    Working network, but how?

--- src/server/network.py
+++ src/server/network.py
@@ -1,9 +1,8 @@
 import ctypes
+import time
 
-# Location of network DLL
 cent = ctypes.CDLL("../network/cent.so")
 
-# Functions to copy from DLL
 StartCentServer = cent.StartCentServer;
 StartCentServer.restype = ctypes.c_void_p;
 CreateCent = cent.CreateCent;
@@ -16,63 +15,39 @@ CentGetDataFromCent_Float = cent.CentGetDataFromCent;
 CentGetDataFromCent_Float.argtypes = [ ctypes.c_void_p ];
 CentGetDataFromCent_Float.restype = ctypes.POINTER(ctypes.c_float)
 
-# types
-CENTCB = ctypes.CFUNCTYPE( ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p );
-
 def my_callback_function( conn, data, ids ):
         fields = CentGetDataFromCent_Float( data );
+        print (fields[0])
+        print (fields[1])
+        print (ids)
 
-# Set the server ip address and port here
+CENTCB = ctypes.CFUNCTYPE( ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p );
 server_handle = StartCentServer(b"0.0.0.0", 8553);
-CentServerAddCB( server_handle, b"/*", CENTCB(my_callback_function), 44 );
 
-compatBytes = bytes
-try:
-  bytes("foo", 'ascii')
-except TypeError:
-  def compatBytes(string, enc=None):
-    return bytes(string)
+gencallback = CENTCB(my_callback_function)
+CentServerAddCB( server_handle, b"/*", gencallback, 44 )
 
-# c = CreateCent( "/hi", CENTFLAGS_OK, CENTCODES_FLOATS, 8, (void*)fvs2 );
+#CentServerAddCB( server_handle, b"/*", CENTCB(my_callback_function), 44 )
 
-# This is used to send a packet to the clients to update entity information
+
 def update(entityType, name, data):
-  print (data.string())
-  cent = CreateCent(data.type(), 0x80, 1, data.length(), data.string())
-  ChangeValue( server_handle, cent, 1 );
+  fields = ctypes.c_float * 3;
+  myvar = fields(*data.contents);
+  ChangeValue( server_handle, CreateCent(data.type(), 0x80, 1, 12, myvar ), 1 );
+  time.sleep(.01)
   
-# This is used to convert python datatypes to the network types we use
 class data:
   def __init__(self, dataType, contents):
     self.dataType = dataType
     self.contents = contents
   def string(self):
     if self.dataType == "loc":  
-      floats = ctypes.c_float * 3
-      return floats(*self.contents)
+      floats = ctypes.c_float * 2
+      #return floats(*self.contents)
+      return floats(*[1,2])
   def type(self):
     if self.dataType == "loc":
-      return b"/loc"
+      return b"/hi"
   def length(self):
     if self.dataType == "loc":
-      return 12
-
-      
-    
-
-#Network system loadout:
-
-
-#s = strike craft
-#u = unmanned thing (i.e. missile)
-#c = corvette ship
-
-
-#i.e.
-
-#s0/pvr  [posx posy posz velx vely velz rotW rotX rotY rotZ (all floats)]
-#s0/pwr  [power (energy) (float)]
-#s0/name [ship human-readable name (string)]
-#u0/mod  [ship model] (string)
-#c3/hull [remaining hull]
-#c3/bang [posx posy posz size] (positions in global) size in meters  (Causes appearance of explosion)
+      return 8

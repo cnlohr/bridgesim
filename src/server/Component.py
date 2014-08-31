d0ce0241c5724b51fb76ef12775cda29dd7ed825
Dylan Whichard <dylan@whichard.com>
2014-08-31 03:58:00 -0400

    Add shield generator

--- src/server/Component.py
+++ src/server/Component.py
@@ -7,6 +7,7 @@ class Component:
         self.model = "ball"
         self.hp = 1
         self.mass = 1000
+        self.radius = 1
         self.position = Vector()
         self.orientation = Vector(1, 0, 0)
         self.energy = 0.0
@@ -19,6 +20,14 @@ class Component:
             return .1
         else:
             return 0
+
+    def takeDamage(self, amount):
+        applied = min(self.hp, amount)
+        self.hp -= applied
+        return amount - applied
+
+    def isDead(self):
+        return hp == 0
 
     def tick(self, duration):
         return {}
@@ -100,6 +109,44 @@ class WeaponsStation(Component):
             if self.loadTime >= self.payload.loadTime:
                 self.loadStatus = "Empty"
                 self.payload = None
+
+class ShieldGenerator(Component):
+    def __init__(self, ship, config):
+        super.__init__(self, ship, config)
+
+        self.baseRadius = self.radius
+        self.enabled = False
+
+    def takeDamage(self, amount):
+        if self.enabled:
+            applied = min(amount, self.shieldHp)
+            self.shieldHp -= applied
+            amount -= applied
+
+        if self.shieldHp <= 0 or not self.enabled:
+            applied = min(amount, self.hp)
+            self.hp -= applied
+            amount -= applied
+
+        return amount
+
+    def enable(self):
+        self.radius = self.shieldRadius
+        self.enabled = True
+
+    def disable(self):
+        self.radius = self.baseRadius
+        self.enabled = False
+
+    def energyNeeded(self):
+        if self.enabled:
+            return 1
+        else:
+            return .2
+
+    def tick(self, duration):
+        if self.hp > 0:
+            self.shieldHp += self.shieldRecharge * duration * self.energy
 
 COMPONENT_CLASSES = {
     "Component": Component,

2d5b560352eed3a508f85c7ba32c3d3a2b90f22a
Dylan Whichard <dylan@whichard.com>
2014-08-31 14:09:19 -0400

    Add energy distribution for ships

--- src/server/Ship.py
+++ src/server/Ship.py
@@ -7,9 +7,17 @@ class Ship(Entity):
   def __init__(self, config, universe):
     super().__init__(config, universe)
     self.__dict__.update(config)
+
+    self.energy = self.maxEnergy
+
+    # How much power Engineering is giving to each component - [0, 1] normally
+    self.energySupply = {}
+
     temp = []
     for i in self.components:
-      temp.append(findComponent(i['type'])(self, i))
+      comp = findComponent(i['type'])(self, i)
+      temp.append(comp)
+      self.energySupply[comp] = 1
     self.components = temp
     
   def collide(self, other):
@@ -27,7 +35,19 @@ class Ship(Entity):
       
   def tick(self, duration):
     print("Ticking ship")
+    # Figure out how much each component wants and is being allowed
+    needed = {component: component.energyNeeded() * duration * self.energySupply[component] for component in self.components}
+
+    totalNeeded = sum(needed.values())
+    factor = 1 if totalNeeded <= self.energy else self.energy / totalNeeded
+
+    # do this before looping so nothing grabs power allotted to something else
+    self.energy -= totalNeeded * factor
+
+    print("Consumed", totalNeeded * factor, "power this tick; now", self.energy)
     for i in self.components:
+      i.energy = factor * duration * self.energySupply[i] * needed[i]
+      print("Gave ", i.energy, " power to ", type(i))
       i.tick(duration)
   
   def tock(self):

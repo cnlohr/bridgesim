76e95eea00a8f11d0e135fe628e31aa6bf9fb2c3
Zachary Stewart <chirokidz@gmail.com>
2014-10-01 21:23:02 -0400

    setup basic page

--- src/webgl/testingserver.py
+++ src/webgl/testingserver.py
@@ -15,6 +15,9 @@ if __name__ == '__main__':
   parser.add_argument('--port', '-p', help='Port for the web server', nargs='?', type=int, default=8080)
   parser.add_argument('--bind-interface', '-b', help='IP address to bind to', nargs='?', type=str,
                       default='localhost')
+  parser.add_argument('--debug', '-d', help='Debug mode uses unminified javascript and server debug mode',
+                      action='store_true')
 
   args = parser.parse_args()
-  app.run(host=args.bind_interface, port=args.port, debug=True)
+  app.config['DEBUG'] = args.debug
+  app.run(host=args.bind_interface, port=args.port, debug=args.debug)
